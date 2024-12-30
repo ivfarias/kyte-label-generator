60fe87a7a4b30fc9dcf958cec19e498af53c92b7
ivfarias <imivanfarias@gmail.com>
2024-12-30 12:26:14 -0300

    modified:   src/index.css 	modified:   src/main.tsx 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
